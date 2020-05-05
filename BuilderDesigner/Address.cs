b25e22e3207567fc20d9aa2978c2dee1bcf291d2
1057337494 <1057337494@qq.com>
2020-05-05 17:57:04 +0800

    新建项目

---
+++ BuilderDesigner/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderDesigner
+{
+    public class Address
+    {
+        public Address(AddressBuilder build)
+        {
+            City = build.City;
+            Code = build.Code;
+            Street = build.Street;
+        }
+
+        public string City { get; private set; }
+
+        public string Code { get; private set; }
+
+        public string Street { get; private set; }
+
+
+    }
+}
---
+++ BuilderDesigner/AddressBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuilderDesigner
+{
+    public class AddressBuilder
+    {
+        public string City { get; private set; }
+
+        public string Code { get; private set; }
+
+        public string Street { get; private set; }
+
+
+        public void SetCity(string city) => City = city;
+
+        public void SetCode(string code) => Code = code;
+
+        public void SetStreet(string street) => Street = street;
+
+        public Address Build()
+        {
+            var proList = typeof(AddressBuilder).GetProperties();
+            if (proList.Any(s => s.GetValue(this) == null))
+            {
+                throw new ArgumentNullException($"构造失败：有参数未填写");
+            }
+
+            return new Address(this);
+
+        }
+
+    }
+}
